1bd3d9174989eb65931bcbe100dc2c4492b92daf
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-05-11 12:07:53 +0900

    added the main program file

---
+++ group.r
@@ -0,0 +1,10 @@
+# ----------------------------------------------------------------------------------
+#    statAna 5th wek #9#10  Analysis of Covariance Structures
+#                                                    by T. Kuwahara,  Keio Univ.
+# ----------------------------------------------------------------------------------
+# 作業ディレクトリの設定（自分の環境にあわせて修正して下さい）
+setwd("/home/yutaro/prog/r/grouplast")
+
+# 青木先生（群馬大）による関数の読み込み（自分の環境にあわせて修正して下さい）
+# source("all.R", encoding="EUC-JP")
+source("http://aoki2.si.gunma-u.ac.jp/R/src/all.R", encoding="euc-jp")
